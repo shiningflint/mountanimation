30f3e21587caf70dfe0813281d6a73b9dd1869ee
Adam Christopher B Widjaja <adam.keristoper@gmail.com>
2017-08-30 18:52:18 +0900

    First commit, react install & setup

---
+++ app/components/App.js
@@ -0,0 +1,15 @@
+var React = require('react');
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <h2>Hello World!</h2>
+    );
+  }
+}
+
+module.exports = App;
