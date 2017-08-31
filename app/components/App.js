ec5b1904cb81d36011026394bbe6a9c1214c0088
Adam Christopher B Widjaja <adam.keristoper@gmail.com>
2017-08-31 13:51:58 +0900

    animation on mounting and unmounting

--- app/components/App.js
+++ app/components/App.js
@@ -4,28 +4,58 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.mountStyle = this.mountStyle.bind(this);
+    this.unMountStyle = this.unMountStyle.bind(this);
+    this.transitionEnd = this.transitionEnd.bind(this);
     this.state = {
       show: true,
       style: {
         opacity: 0,
+        maxHeight: 0,
+        margin: 0,
         transition: 'all 1s ease',
       }
     }
   }
 
   componentWillReceiveProps(newProps) {
+    if (!newProps.mounted) {
+      return this.unMountStyle();
+    } else {
+      this.setState({
+        show: true
+      });
+      setTimeout(this.mountStyle, 10);
+    }
+  }
+
+  unMountStyle() {
     this.setState({
-      show: newProps.mounted
-    });
+      style: {
+        opacity: 0,
+        maxHeight: 0,
+        margin: 0,
+        transition: 'all 1s ease',
+      }
+    })
   }
 
   mountStyle() {
     this.setState({
       style: {
         opacity: 1,
+        maxHeight: 200,
+        margin: "21px 0",
         transition: 'all 1s ease',
       }
     });
+  }
+
+  transitionEnd() {
+    if (!this.props.mounted) {
+      this.setState({
+        show: false
+      })
+    }
   }
 
   componentDidMount() {
@@ -35,7 +65,11 @@ class App extends React.Component {
   render() {
     return(
       this.state.show &&
-      <h1 className="title" style={this.state.style}>Hello!</h1>
+      <h1 className="title"
+        style={this.state.style}
+        onTransitionEnd={this.transitionEnd}>
+        Hello!
+      </h1>
     )
   }
 }
